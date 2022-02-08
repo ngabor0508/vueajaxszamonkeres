c03dbf4b646789d55fbca13d8f6939f5e8265c58
ngabor0508 <ngabor0508@gmail.com>
2022-02-08 23:32:25 +0100

    1.feladat: az adatbázisban lévő adatokat litázza

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table class="table_one">
+      <thead>
+        <tr>
+          <th>Azonosító</th>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statues" v-bind:key="statue.id" class="active-row">
+          <td>{{ statue.id }}</td>
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+          <td>
+            <button @click="deleteStatue(statue.id)">Törlés</button>
+            <button @click="editStatue(statue.id)">Szerkesztés</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      mod_new: true, 
+      saving: false,
+      statue: {
+        id: null,
+        person: '',
+        height: '',
+        price: '',
+      },
+      statues: []
+    }
+  },
+  methods: {
+    async loadData () {
+     let Response = await fetch('http://127.0.0.1:8000/api/statues')
+     let data = await Response.json()
+     this.statues = data
+    },
+    
+    
+    resetForm() {
+      this.statue = {
+        id: null,
+        person: '',
+        height: '',
+        price: '',
+      }
+      this.mod_new = true
+    }
+  },
+  mounted() {
+    this.loadData()
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
