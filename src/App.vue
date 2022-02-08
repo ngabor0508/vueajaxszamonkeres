d62df4ddae9869774dc718420b32d0928b46b979
ngabor0508 <ngabor0508@gmail.com>
2022-02-08 23:40:08 +0100

    Új elem létrehozása működik

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,28 @@
           <td>{{ statue.height }}</td>
           <td>{{ statue.price }}</td>
           <td>
-            <button @click="deleteStatue(statue.id)">Törlés</button>
-            <button @click="editStatue(statue.id)">Szerkesztés</button>
+            <button @click="deleteStatue(statue.id)" class="torles">Törlés</button>
+            <button @click="editStatue(statue.id)" class="szerkeszt">Szerkesztés</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="hidden" v-model="statue.id">
+          </td>
+          <td>
+            <input type="text" v-model="statue.person">
+          </td>
+          <td>
+            <input type="number" v-model="statue.height">
+          </td>
+          <td>
+            <input type="number" v-model="statue.price">
+          </td>
+          <td>
+            <button v-if="mod_new" @click="newStatue" :disabled="saving" class="mentes">Mentés</button>
+            <button v-if="mod_new" @click="cancelEdit" :disabled="saving" class="megse">Mégse</button>
+            <button v-if="!mod_new" @click="saveStatue" :disabled="saving" class="mentes">Mentés</button>
+            <button v-if="!mod_new" @click="cancelEdit" :disabled="saving" class="megse">Mégse</button>
           </td>
         </tr>
       </tbody>
@@ -50,8 +70,50 @@ export default {
      let data = await Response.json()
      this.statues = data
     },
-    
-    
+    async deleteStatue(id) {
+      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`, {
+        method: 'DELETE'
+      })
+      console.log(Response)
+      await this.loadData()
+    },
+    async newStatue() {
+      this.saving='disabled'
+     await fetch('http://127.0.0.1:8000/api/statues', {
+       method: 'POST',
+       headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },
+       body: JSON.stringify(this.statue) 
+     })
+     await this.loadData()
+     this.saving=false
+     this.resetForm()
+    },
+    async saveStatue() {
+      this.saving='disabled'
+     await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
+       method: 'PATCH',
+       headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },
+       body: JSON.stringify(this.statue) 
+     })
+     await this.loadData()
+     this.saving=false
+     this.resetForm()
+    },
+    async editStatue(id) {
+      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
+      let data = await Response.json()
+      this.statue = {...data};
+      this.mod_new = false
+    },
+    cancelEdit () {
+      this.resetForm()
+    },
     resetForm() {
       this.statue = {
         id: null,
